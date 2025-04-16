6922afc968bf90d0ac72fe5b1d08bc29f43d4298
Art-MC <armccray@lbl.gov>
2025-04-16 15:47:33 -0700

    adding class properties and np cp support to dataset

--- quantem/core/datastructures/dataset.py
+++ quantem/core/datastructures/dataset.py
@@ -1,17 +1,12 @@
-from typing import TYPE_CHECKING
-
 import numpy as np
 
 from quantem.core import config
 from quantem.core.io.serialize import AutoSerialize
 
-if TYPE_CHECKING:
+if config.get("has_cupy"):
     import cupy as cp
 else:
-    if config.get("has_cupy"):
-        import cupy as cp
-    else:
-        import numpy as cp
+    import numpy as cp
 
 
 # base class for quantem datasets
@@ -26,26 +21,11 @@ class Dataset(AutoSerialize):
         signal_units: str | None = None,
     ):
         self.array = data
-        if name is None:
-            self.name = f"{data.ndim}d dataset"
-        else:
-            self.name = name
-        if origin is None:
-            self.origin = np.zeros(data.ndim)
-        else:
-            self.origin = origin
-        if sampling is None:
-            self.sampling = np.ones(data.ndim)
-        else:
-            self.sampling = sampling
-        if units is None:
-            self.units = ["pixels"] * data.ndim
-        else:
-            self.units = units
-        if signal_units is None:
-            self.signal_units = "arb. units"
-        else:
-            self.signal_units = signal_units
+        self.name = f"{data.ndim}d dataset" if name is None else name
+        self.origin = np.zeros(data.ndim) if origin is None else origin
+        self.sampling = np.zeros(data.ndim) if sampling is None else sampling
+        self.units = ["pixels"] * data.ndim if units is None else units
+        self.signal_units = "arb. units" if signal_units is None else signal_units
 
     # Properties
     @property
@@ -54,12 +34,70 @@ class Dataset(AutoSerialize):
 
     @array.setter
     def array(self, arr):
-        if isinstance(arr, (np.ndarray, cp.ndarray)):
+        if isinstance(arr, np.ndarray):
             self._array = arr
+        elif config.get("has_cupy"):
+            if isinstance(arr, cp.ndarray):
+                self._array = arr
         elif isinstance(arr, (list, tuple)):
             self._array = np.array(arr)
         else:
             raise TypeError
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, val: str):
+        self._name = str(val)
+
+    @property
+    def origin(self) -> np.ndarray:
+        return self._origin
+
+    @origin.setter
+    def origin(self, val: np.ndarray | list):
+        origin = np.array(val)
+        if origin.ndim != self.ndim:
+            raise ValueError(
+                f"Got origin dimension {origin.ndim} which does not match data dimension {self.ndim}"
+            )
+        self._origin = origin
+
+    @property
+    def sampling(self) -> np.ndarray:
+        return self._sampling
+
+    @sampling.setter
+    def sampling(self, val: np.ndarray | list):
+        sampling = np.array(val)
+        if sampling.ndim != self.ndim:
+            raise ValueError(
+                f"Got sampling dimension {sampling.ndim} which does not match data dimension {self.ndim}"
+            )
+        self._sampling = sampling
+
+    @property
+    def units(self) -> list[str]:
+        return self._units
+
+    @units.setter
+    def units(self, val: np.ndarray | list):
+        units = [str(v) for v in val]
+        if len(units) != self.ndim:
+            raise ValueError(
+                f"Got units dimension {len(units)} which does not match data dimension {self.ndim}"
+            )
+        self._units = units
+
+    @property
+    def signal_units(self) -> str:
+        return self._signal_units
+
+    @signal_units.setter
+    def signal_units(self, val: str):
+        self._signal_units = str(val)
 
     @property
     def shape(self):
